f6a80043aaefac103441e5fed4c5be5f3089e1a2
ChaseLittlepaws <littlepaws@hey.com>
2023-02-22 13:16:15 -0500

    Completed triangle program with turtle library

---
+++ turtleTriangle.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.color("Yellow")
+
+small = 25
+angle = 120
+
+turtle.forward(small)
+turtle.left(angle)
+turtle.forward(small)
+turtle.left(angle)
+turtle.forward(small)
+turtle.left(angle)
+
+turtle.Screen().exitonclick()
