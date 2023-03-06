592f9dc9b73f909a35faa60b3713719d463d210d
ChaseLittlepaws <littlepaws@hey.com>
2023-03-06 10:53:16 -0500

    Added comments

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
+# A program to test the capabilities of Python
+# Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
+
 print("Hello World")
 print("Challenge #", 2 + 3)
--- turtleTriangle.py
+++ turtleTriangle.py
@@ -1,3 +1,6 @@
+# A program to draw a triangle without loops using the turtle library
+# Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
+
 import turtle
 
 turtle.color("Yellow")
