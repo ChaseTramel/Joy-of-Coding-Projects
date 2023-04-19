e20de3d51f7144d215e43963c034eabe47e48f4c
ChaseLittlepaws <littlepaws@hey.com>
2023-04-19 12:07:52 -0400

    Took Quiz 2

---
+++ Module One/forLoopQuiz.py
@@ -0,0 +1,31 @@
+# A program to assess my learning about for loops
+# Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
+
+for i in "oompah!":
+    print(i.upper())
+print("\n \n")
+
+list = [0,1,4,16,64,256]
+for j in list:
+    print(j, end = " ")
+print("\n \n")
+
+for k in "scream":
+    if k == "m":
+        print(k.upper(), end = " for ice cream!")
+    else:
+        print(k.upper(), end = "-")
+print("\n \n")
+
+sum = 0
+for l in range(-400, 401, 2):
+    sum += l
+print(sum)
+print("\n \n")
+
+for m in "HALLELUJAH!":
+    if m == "!":
+        print(m, end = "")
+    else:
+        print(m, end = "--")
+print("\n \n")
