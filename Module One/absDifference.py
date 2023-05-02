8a080440489f0e1de6e786dc5a6703711c29e87d
ChaseLittlepaws <littlepaws@hey.com>
2023-05-01 20:02:22 -0400

    Created absDifference and pyramid as an assignment.

---
+++ Module One/absDifference.py
@@ -0,0 +1,14 @@
+# A python program that takes two numbers and returns their absolute difference
+# # Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
+
+def absDifference(x, y):
+    if (x == y):
+        return 0
+    else:
+        nums = [x, y]
+        diff = max(nums) - min(nums)
+        return abs(diff)
+    
+print(absDifference(5, 10))
+print(absDifference(10, 5))
+print(absDifference(200, -200))
---
+++ Module One/pyramid.py
@@ -0,0 +1,9 @@
+# A python program that creates an ASCII pyramid
+# Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
+
+def pyramid(symbol, number):
+    for i in range(number):
+        print(symbol * (i + 1))
+    return
+
+pyramid("x", 10)
