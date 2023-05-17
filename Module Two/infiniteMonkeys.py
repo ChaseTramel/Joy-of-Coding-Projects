4f3ff4162ce10b9b41f4d7c001285b1e5e111f72
ChaseLittlepaws <littlepaws@hey.com>
2023-05-17 10:39:11 -0400

    Added support for more complex quotes.

--- Module Two/infiniteMonkeys.py
+++ Module Two/infiniteMonkeys.py
@@ -1,12 +1,12 @@
 # A Python program that generates a random string and compares it to a goal string
-# The program compares the generated string to the goal string to create a score, and keeps the best score
 # The program also keeps letters that are in the correct spaces, generating only randoms letters for the remaining spaces
 # Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
 
+import string
 import random
 
 def generateGuess(length):
-    letters = "abcdefghijklmnopqrstuvwxyz "
+    letters = string.printable
     testList = [""] * length  # init testList as list of empty strings goalList long
     global correctGuess
     for i in range(length):  
@@ -20,7 +20,7 @@ def generateGuess(length):
 def compareGuess(test, goal, correctGuess):
     correctLetters = 0
     for j in range(len(test)):
-        if test[j] == goal[j]:  #if test list letter  is correct
+        if test[j] == goal[j]:  #if test list letter is correct
             correctLetters += 1  #count the correct letter
             if correctGuess[j] == "": correctGuess[j] = test[j]  # if the correct guess is empty and the test list item is correct, assign the correct guess to that list
     currentScore = correctLetters / len(goal)  
@@ -34,8 +34,8 @@ def trackScore(current):
     global best
     if current > best: best = current 
 
-
-goalList = list("methinks it is like a weasel") # to be easily changed. init as a list
+quote = "To be, or not to be: that is the question.".casefold()
+goalList = list(quote) # init as a list of each letter
 length = len(goalList)
 
 correctGuess = ["" for _ in goalList] # init correctGuess as a list of empty strings as global variable
