23f05a3382cd820fcea89c1c92ae7682da9e00fa
ChaseLittlepaws <littlepaws@hey.com>
2023-05-17 11:08:00 -0400

    Added a guess counter. Added more dialogue.

--- Module Two/infiniteMonkeys.py
+++ Module Two/infiniteMonkeys.py
@@ -8,6 +8,8 @@ import random
 def generateGuess(length):
     letters = string.printable  #a string of all printables chars, including punctuation
     testList = [""] * length  # init testList as list of empty strings goalList long
+    global guesses
+    guesses += 1
     global correctGuess
     for i in range(length):  
         if correctGuess[i] == "":
@@ -17,15 +19,16 @@ def generateGuess(length):
     print("Monkey says I'll guess this:", "".join(testList))
     return testList
 
-def compareGuess(test, goal, correctGuess):
+def compareGuess(test, goal, correctGuess, guesses):
     correctLetters = 0
     for j in range(len(test)):
         if test[j] == goal[j]:  #if test list letter is correct
             correctLetters += 1  #count the correct letter
             if correctGuess[j] == "": correctGuess[j] = test[j]  # if the correct guess is empty and the test list item is correct, assign the correct guess to that list
     currentScore = round(correctLetters / len(goal), 2)
+    print("Computer says you have guessed " + str(guesses) + " times.")
     print("Computer says you guessed " + str(correctLetters) + " correct letter(s).")
-    print("Computer says you're " + str(currentScore * 100) + " percent accurate.")
+    print("Computer says you are " + str(round(currentScore * 100)) + " percent accurate.")
     trackScore(currentScore)  # use this function to update best
     print("Monkey says I know these are right:", "".join(correctGuess))
     return correctGuess
@@ -42,6 +45,9 @@ correctGuess = ["" for _ in goalList] # init correctGuess as a list of empty str
 print(correctGuess)
 
 best = 0
+guesses = 0
 
-while best != 1: 
-    correctGuess = compareGuess(generateGuess(length), goalList, correctGuess) # correct guess gets the output of compareString
+while best != 1:
+    correctGuess = compareGuess(generateGuess(length), goalList, correctGuess, guesses) # correct guess gets the output of compareString
+print("Computer says congratulations Monkey, you guessed '" + quote + "' correctly after " + str(guesses) + " guesses.")
+print("Monkey celebrates!")
