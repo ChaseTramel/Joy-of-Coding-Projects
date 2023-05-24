1331567c86e87100408af3df6b0243ad73e9023d
ChaseLittlepaws <littlepaws@hey.com>
2023-05-24 09:54:03 -0400

    Completed matching braces assignment

--- Data Structures & Algorithms/librarySystem.py
+++ Data Structures & Algorithms/librarySystem.py
@@ -2,23 +2,39 @@
 # Chase Tramel aka Kasey Chase Littlepaws - https://github.com/ChaseLittlepaws
 
 import datetime
+import random
+import string
 
 class Book:
-    def __init__(self, name, year, author, dueDate):
-        self.name = name
-        self.year = year
+    def __init__(self, title, publishedYear, author):
+        self.title = title
+        self.publishedYear = publishedYear
         self.author = author
         self.dueDate = None
-        self.renter = None
+        self.rentingPatron = None
     def __str__(self):
-        return f"{self.name} by {self.author} is due on {self.dueDate}"
-    def checkOut(self, renter):
+        return f"{self.title} by {self.author}, published in {self.publishedYear}, was added to the system."
+    def checkOut(self, patronName, patronID):
         today = datetime.date.today()
-        self.renter = renter
+        self.rentingPatronName = patronName
+        self.rentingPatronID = patronID
         self.dueDate = today + datetime.timedelta(days = 14)
-        print(f"Thanks {self.renter} for checking out {self.name}. This book is due on {self.dueDate}")
+        print(f"Thanks, {self.rentingPatronName} (ID Number: {self.rentingPatronID}), for checking out {self.title}. This book is due on {self.dueDate}.")
 
-gatsby = Book("The Great Gatsby", "1925", "Gatsby Author", datetime.date(2023, 5, 28))
+class Patron:
+    def __init__(self, name, birthday):
+        self.name = name
+        self.birthday = birthday
+        self.IDNumber = (''.join(random.choices(string.digits, k=10)))
+    def __str__(self):
+        return f"Thank you for registering {self.name}, DOB: {self.birthday}. Their ID Number is {self.IDNumber}."
+
+class Librarian:
+    pass
+
+gatsby = Book("The Great Gatsby", "1925", "F. Scott Fitzgerald")
 
 print(gatsby)
-gatsby.checkOut("Chase")
+newPatron = Patron("Chase", datetime.date.today())
+print(newPatron)
+gatsby.checkOut(newPatron.name, newPatron.IDNumber)
