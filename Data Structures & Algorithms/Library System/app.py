d2f66f3e73784f695be942278df4c47c7153b70a
ChaseLittlepaws <littlepaws@hey.com>
2023-06-06 15:15:19 -0400

    Refactored fine system into Patron class

--- Data Structures & Algorithms/Library System/app.py
+++ Data Structures & Algorithms/Library System/app.py
@@ -21,12 +21,10 @@ class Library:
     def __init__(self):
         self.books = [] # List of book objects
         self.patrons = [] # List of patron objects
-        self.fines = {} # Dictionary to associate fines with patrons
     def saveData(self):
         data = {
             'books': self.books,
-            'patrons': self.patrons,
-            'fines': self.fines
+            'patrons': self.patrons
         }
         with open("libraryFile.txt", 'wb') as file:
             pickle.dump(data,file)
@@ -37,7 +35,6 @@ class Library:
                 data = pickle.load(file)
             self.books = data['books']
             self.patrons = data['patrons']
-            self.fines = data['fines']
             print("Library data loaded successfully.")
         except FileNotFoundError:
             print("Library file not found!")
@@ -76,10 +73,26 @@ class Library:
         for book in self.books:
             if book.dueDate is None:
                 print(f"Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}")
+    def addPatron(self, patron):
+        if any(existingPatron.name == patron.name for existingPatron in self.patrons):
+            print(f"Sorry, someone with the name {patron.name} already exists in the library system.")
+        else:
+            self.patrons.append(patron)
+            print(f"Thank you for registering {patron.name}. Their ID Number is {patron.IDNumber}.")
+        self.saveData
     def displayAllPatrons(self):
         print("All Patrons:")
         for patron in self.patrons:
             print(f"Patron Name: {patron.name}, Patron Date of Birth: {patron.birthday}, Patron ID: {patron.IDNumber}")
+    def displayPatronFines(self):
+        print("All Patrons with Fines")
+        for patron in self.patrons:
+            print(f"Patron Name: {patron.name}, Fine: {patron.fine}")
+    def IDExists(self, IDNumber):
+        for patron in self.patrons:
+            if patron.IDNumber == IDNumber:
+                return True
+        else: return False
 class Book:
     def __init__(self, title, publishedYear, author):
         self.title = title
@@ -87,22 +100,23 @@ class Book:
         self.author = author
         self.dueDate = None
         self.rentingPatron = None
-        self.fine = 0 # Initilized with no fine
         print(f"{self.title} by {self.author}, published in {self.publishedYear}, arrived.")
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.publishedYear}"
-    def checkOut(self, patronName, patronID):
-        if patronID in library.fines and library.fines[patronID] > 0:
-            print(f"Sorry {patronName}, you can't check out {self.title} because you have a fine of ${library.fines[patronID]:.2f}")
+    def checkOut(self, patron):
+        if patron.fine > 0:
+            print(f"Sorry {patron.name}, you can't check out {self.title} because you have a fine of ${patron.fine:.2f}")
+        elif patron not in library.patrons:
+            print(f"Sorry {patron.name}, you can't check out {self.title} because you're not in the libary system.")
         elif self.dueDate == None:
             today = datetime.date.today()
-            self.rentingPatronName = patronName
-            self.rentingPatronID = patronID
+            self.rentingPatronName = patron.name
+            self.rentingPatronID = patron.IDNumber
             self.dueDate = today + datetime.timedelta(days = 14)
             print(f"Thanks, {self.rentingPatronName} (ID Number: {self.rentingPatronID}), for checking out {self.title}. This book is due on {self.dueDate}.")
+            library.saveData()
         else:
-            print(f"Sorry, {patronName}, {self.title} is checked out. It should be returned on or before {self.dueDate}.")
-        library.saveData()
+            print(f"Sorry, {patron.name}, {self.title} is checked out. It should be returned on or before {self.dueDate}.")
     def returnBook(self):
         if self.dueDate is not None:
             self.calculateFine()
@@ -134,32 +148,28 @@ class Book:
             print(f"{self.title} by {self.author}, published in {self.publishedYear}, is currently checked out and due {self.dueDate}.")
 
 class Patron:
-    def __init__(self, name, birthday):
+    def __init__(self, name, birthMonth, birthDay, birthYear):
         self.name = name
-        self.birthday = birthday
-        self.IDNumber = (''.join(random.choices(string.digits, k=6)))
+        self.birthday = datetime.date(birthYear, birthMonth, birthDay)
+        self.IDNumber = self.generateIDNumber()
         self.fine = 0
-        library.patrons.append(self)
-    def __str__(self):
-        return f"Thank you for registering {self.name}, DOB: {self.birthday}. Their ID Number is {self.IDNumber}."
+        library.addPatron(self)
+    def generateIDNumber(self):
+        while True:
+            IDNumber = (''.join(random.choices(string.digits, k=6)))
+            if not library.IDExists(IDNumber):
+                return IDNumber
     def payFine(self, payment):
-        if self.IDNumber in library.fines:  # If the patron is in the fine dictionary
-            if library.fines[self.IDNumber] > 0:
-                print(f"Thanks {self.name}, you paid your fine of ${library.fines[self.IDNumber]:.2f}.")
-                library.fines[self.IDNumber] -= payment
-                print(f"Your balance is now ${library.fines[self.IDNumber]:.2f}.")
-                if library.fines[self.IDNumber] < 0:  # if Patron gave too much money
-                    change = abs(library.fines[self.IDNumber])
-                    print(f"Here is your change of ${change:.2f}")
-                    library.fines[self.IDNumber] = 0
-                    print(f"Your balance is now ${library.fines[self.IDNumber]:.2f}.")
-            else:
-                print(f"Thanks {self.name}, you contributed towards your fine of ${library.fines[self.IDNumber]:.2f}.")
-                library.fines[self.IDNumber] -= payment
-                print(f"Your balance is now ${library.fines[self.IDNumber]:.2f}.")
-        else:
-            print(f"{self.name}, you didn't have any fines to pay!")
-        library.saveData()
+        print(f"{self.name}, you have a fine of ${self.fine:.2f}")
+        if self.fine > 0:
+            self.fine -= payment
+            print(f"You paid ${payment:.2f} towards you fine balance. Your balance is now ${self.fine:.2f}.")
+            if self.fine < 0:
+                change = abs(self.fine)
+                print(f"Here's your change of ${change:.2f}")
+                self.fine = 0
+                print(f"Your balance is now ${self.fine:.2f}.")
+            library.saveData()
 
 
 
@@ -167,30 +177,22 @@ class Patron:
 library = Library()
 library.loadData()
 
-# Display all of the books in the system
-library.displayAllBooks()
-print("\n")
-
-# Display the books that by status
-library.displayBooksByStatus()
-print("\n")
-
-# Display all patrons
-library.displayAllPatrons()
-print("\n")
-
-# Display all patron balances
-library.displayPatronBalances()
-print("\n")
-
-# Add a book to the system
 hero = Book("Hero on a Mission: A Path to a Meaningful Life", "2023", "Donald Miller")
 print("\n")
+
 library.addBook(hero)
 print("\n")
 
-# Display all of the books in the system
 library.displayAllBooks()
+print("\n")
+
+chase = Patron("Chase Tramel", 3, 15, 1995)
+print("\n")
+
+library.displayBooksByStatus()
+print("\n")
+
+library.displayAllPatrons()
 print("\n")
 
 if __name__ == '__main__':
