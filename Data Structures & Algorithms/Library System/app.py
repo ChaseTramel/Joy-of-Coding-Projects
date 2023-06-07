1e5bc6e98c7e66eaac299b0477a9375fc87e2f37
ChaseLittlepaws <littlepaws@hey.com>
2023-06-07 10:59:14 -0400

    Implemented CRUD in app.py (formally librarySystem.py)

--- Data Structures & Algorithms/Library System/app.py
+++ Data Structures & Algorithms/Library System/app.py
@@ -37,12 +37,20 @@ class Library:
                 data = json.load(file)
             self.books = [Book(**book) for book in data['books']]
             self.patrons = [Patron(**patron) for patron in data['patrons']]
+            for book in self.books:
+                if book.rentingPatron:
+                    patron_id = book.rentingPatron
+                    for patron in self.patrons:
+                        if patron.IDNumber == patron_id:
+                            book.rentingPatron = patron.name
+                            break
             print("Library data loaded successfully!")
         except FileNotFoundError:
             print("Library data not found. A new library file was made.")
         except json.JSONDecodeError:
             print("Error loading the library data. The file may be corrupted.")
-    def addBook(self, book):
+    def addBook(self, title, pubYear, author, dueDate, rentingPatron, bookID):
+        book = Book(title, pubYear, author, dueDate, rentingPatron, bookID)
         if any(existingBook.title == book.title for existingBook in self.books):
             print(f"Sorry, a book with the title '{book.title}' is already in the library.")
         else: 
@@ -65,23 +73,23 @@ class Library:
     def displayAllBooks(self):
         print("All Libary Books:")
         for book in self.books:
-            print(f"Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}")
+            print(f"BookID: {book.bookID}, Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}")
     def displayBooksByStatus(self):
         print("Books Checked Out:")
         for book in self.books:
             if book.dueDate is not None:
-                print(f"Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}, Due: {book.dueDate}")
+                print(f"BookID: {book.bookID}, Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}, Due: {book.dueDate}")
         print("Books Not Checked Out:")
         for book in self.books:
             if book.dueDate is None:
-                print(f"Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}")
+                print(f"BookID: {book.bookID}, Title: {book.title}, Author: {book.author}, Published Year: {book.publishedYear}")
     def addPatron(self, patron):
         if any(existingPatron.name == patron.name for existingPatron in self.patrons):
             print(f"Sorry, someone with the name {patron.name} already exists in the library system.")
         else:
             self.patrons.append(patron)
             print(f"Thank you for registering {patron.name}. Their ID Number is {patron.IDNumber}.")
-        self.saveData
+            self.saveData()
     def displayAllPatrons(self):
         print("All Patrons:")
         for patron in self.patrons:
@@ -90,29 +98,61 @@ class Library:
         print("All Patrons with Fines")
         for patron in self.patrons:
             print(f"Patron Name: {patron.name}, Fine: {patron.fine}")
-    def IDExists(self, IDNumber):
+    def patronIDExists(self, IDNumber):
         for patron in self.patrons:
             if patron.IDNumber == IDNumber:
                 return True
         else: return False
+    def bookIDExists(self, bookID):
+        for book in self.books:
+            if book.bookID == bookID:
+                return True
+        else: return False
+    def findPatron(self, IDNumber):
+        for patron in self.patrons:
+            if patron.IDNumber == IDNumber:
+                print(f"{patron.name} found!")
+                return patron
+        print("Patron not found.")
+        return None
+    def findBook(self, bookID):
+        for book in self.books:
+            if book.bookID == bookID:
+                print(f"{book.title} found!")
+                return book
+        print("Book not found")
+        return None
 class Book:
-    def __init__(self, title, publishedYear, author, dueDate = None, rentingPatron = None):
+    def __init__(self, title, publishedYear, author, dueDate, rentingPatron, bookID):
         self.title = title
         self.publishedYear = publishedYear
         self.author = author
         self.dueDate = dueDate
         self.rentingPatron = rentingPatron
+        self.bookID = bookID
+        if bookID == None:
+            self.bookID = self.generateBookID()
         print(f"{self.title} by {self.author}, published in {self.publishedYear}, arrived.")
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.publishedYear}\nChecked out by {self.rentingPatron}"
-    def checkOut(self, patron):
+        return json.dumps(dict(self), ensure_ascii=False)
+    def __repr__(self):
+        return self.__str__()
+    def serialize(self):
+        return self.__str__
+    def generateBookID(self):
+        while True:
+            attemptedID = (''.join(random.choices(string.digits, k=8)))
+            if not library.bookIDExists(attemptedID):
+                return attemptedID
+    def checkOut(self, IDNumber):
+        patron = library.findPatron(IDNumber)
         if patron.fine > 0:
             print(f"Sorry {patron.name}, you can't check out {self.title} because you have a fine of ${patron.fine:.2f}")
         elif patron not in library.patrons:
             print(f"Sorry {patron.name}, you can't check out {self.title} because you're not in the libary system.")
         elif self.dueDate == None:
             today = datetime.date.today()
-            self.rentingPatron = patron
+            self.rentingPatron = patron.name
             self.dueDate = today + datetime.timedelta(days = 14)
             print(f"Thanks, {patron.name} (ID Number: {patron.IDNumber}), for checking out {self.title}. This book is due on {self.dueDate}.")
             library.saveData()
@@ -142,16 +182,16 @@ class Book:
 class Patron:
     def __init__(self, name, birthday, IDNumber, fine = 0):
         self.name = name
-        self.birthday = datetime.data(birthday)
-        self.IDNumber = IDNumber
+        self.birthday = birthday
         self.fine = fine
-        library.addPatron(self)
+        self.IDNumber = IDNumber
         if self.IDNumber == None:
             self.IDNumber = self.generateIDNumber()
+        library.addPatron(self)
     def generateIDNumber(self):
         while True:
             IDNumber = (''.join(random.choices(string.digits, k=6)))
-            if not library.IDExists(IDNumber):
+            if not library.patronIDExists(IDNumber):
                 return IDNumber
     def payFine(self, payment):
         print(f"{self.name}, you have a fine of ${self.fine:.2f}")
@@ -170,29 +210,19 @@ class Patron:
 # Initialize Library
 library = Library()
 library.loadData()
-
-dracula = Book("Dracula", "1897", "Bram Stoker")
-library.addBook(dracula)
 print("\n")
 
-
-library.displayAllBooks()
+library.addBook("Frankenstein", "1818", "Mary Shelley", None, None, None)
 print("\n")
 
-olivia = Patron("Olivia Smith", "4-17-2000", None)
-print("\n")
-
-dracula.checkOut(olivia)
+Patron("Chase Tramel", "3-13-1994", None)
 print("\n")
 
 library.displayBooksByStatus()
 print("\n")
 
-library.displayAllPatrons()
-print("\n")
+library.findBook("55772265").checkOut("582053")
 
-print(dracula)
-print("\n")
 
 if __name__ == '__main__':
     app.run()
